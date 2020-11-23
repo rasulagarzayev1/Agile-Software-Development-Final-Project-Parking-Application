6c531ca050a5ae80b231ed4112c7ca2492ed2c05
rasulagarzayev1 <agarzayev.rasul@gmail.com>
2020-11-24 01:34:20 +0200

    create user tdd added

---
+++ test/agileparking_web/controllers/users_controller_test.exs
@@ -0,0 +1,10 @@
+defmodule AgileparkingWeb.UsersControllerTest do
+  use AgileparkingWeb.ConnCase
+
+
+  test "POST /users", %{conn: conn} do
+    conn = post conn, "/users", %{user: [name: "fred1", email: "fred12@gmail.com", license_number: "1234567676", password: "parool" ]}
+    conn = get conn, redirected_to(conn)
+    assert html_response(conn, 200) =~ ~r/User created successfully./
+  end
+end
