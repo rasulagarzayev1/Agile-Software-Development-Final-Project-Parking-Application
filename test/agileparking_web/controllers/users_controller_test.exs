db1dcdb1665a282e3bf390e270056a7366aaa47c
rasulagarzayev1 <agarzayev.rasul@gmail.com>
2020-11-24 03:11:35 +0200

    User login TDD added

--- test/agileparking_web/controllers/users_controller_test.exs
+++ test/agileparking_web/controllers/users_controller_test.exs
@@ -2,8 +2,8 @@ defmodule AgileparkingWeb.UsersControllerTest do
   use AgileparkingWeb.ConnCase
 
 
-  test "POST /users", %{conn: conn} do
-    conn = post conn, "/users", %{user: [name: "fred1", email: "fred12@gmail.com", license_number: "1234567676", password: "parool" ]}
+  test "Test registration", %{conn: conn} do
+    conn = post conn, "/users", %{user: [name: "fred1", email: "farid@gmail.com", license_number: "1234567676", password: "parool" ]}
     conn = get conn, redirected_to(conn)
     assert html_response(conn, 200) =~ ~r/User created successfully./
   end
