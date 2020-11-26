3287037018de66f7eb46f840898264d5cde1d8f2
Orkhan Jamaladdinov <orxan.camaleddinov@gmail.com>
2020-11-26 21:00:28 +0200

    Changes made

--- test/agileparking_web/controllers/sessions_controller_test.exs
+++ test/agileparking_web/controllers/sessions_controller_test.exs
@@ -3,11 +3,19 @@ defmodule AgileparkingWeb.SessionsControllerTest do
   alias Agileparking.Repo
   alias Agileparking.Accounts.User
 
-  test "Test login", %{conn: conn} do
-    conn = post conn, "/users", %{user: [name: "Liivi 2", email: "muuseumi@gmail.com", license_number: "dfghjgkfldks", password: "1234567" ]}
-    conn = post conn, "/sessions", %{session: [email: "muuseumi@gmail.com", password: "1234567" ]}
+  test "Test login success", %{conn: conn} do
+    conn = post conn, "/users", %{user: [name: "Farid1", email: "farid1@gmail.com", license_number: "123456789", password: "1234567" ]}
+
+
+    conn = post conn, "/sessions", %{session: [email: "farid1@gmail.com", password: "1234567" ]}
     conn = get conn, redirected_to(conn)
+
     assert conn.resp_body =~ ~r/Welcome/
   end
 
+  test "Test login failure", %{conn: conn} do
+
+    conn = post conn, "/sessions", %{session: [email: "farid", password: "1234567" ]}
+    assert conn.resp_body =~ ~r/Bad User Credentials/
+  end
 end
