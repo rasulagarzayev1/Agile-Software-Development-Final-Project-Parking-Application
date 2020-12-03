772ca740889b584a4f07bf6b18a5e58fe8294dce
Orkhan Jamaladdinov <orxan.camaleddinov@gmail.com>
2020-12-03 21:51:38 +0200

    Added booking

--- lib/agileparking_web/controllers/zone_controller.ex
+++ lib/agileparking_web/controllers/zone_controller.ex
@@ -1,5 +1,6 @@
 defmodule AgileparkingWeb.ZoneController do
     use AgileparkingWeb, :controller
+    import Ecto.Query
     alias Agileparking.Repo
     alias Agileparking.Sales.Zone
 
@@ -16,7 +17,7 @@ defmodule AgileparkingWeb.ZoneController do
         IO.inspect distance
         Enum.at(distance,0)
     end
-  
+
     def index(conn, _params) do
         render(conn, "index.html", zones: [])
     end
@@ -42,4 +43,35 @@ defmodule AgileparkingWeb.ZoneController do
             render(conn, "index.html", zones: zones)
         end
       end
+
+      def edit(conn, %{"id" => id}) do
+        zone = Repo.get!(Zone, id)
+        changeset = Zone.changeset(zone, %{})
+
+        render(conn, "edit.html", zone: zone, changeset: changeset)
+      end
+
+      def update(conn, %{"id" => id, "zone" => zone_params}) do
+        zone = Repo.all(from z in Zone, where: z.id == ^id)
+        IO.puts("-------------------")
+        IO.inspect(zone)
+        IO.puts("-------------------")
+
+        map = %{}
+        map = Map.put(map, :name, "123")
+        map = Map.put(map, :hourlyPrice, 1)
+        map = Map.put(map, :realTimePrice, 1)
+        map = Map.put(map, :vacant, 1)
+        IO.puts("-------------------")
+        IO.inspect(map)
+        IO.puts("-------------------")
+        changeset = Zone.changeset(zone, map)
+        Repo.update!(changeset)
+        conn
+            |> put_flash(:info, "Balance is increased")
+            |> redirect(to: Routes.user_path(conn, :index))
+
+      end
+
+      def sub(a, b), do: a - b
   end
