bc83a2c9a929925ecf21323e38f0aff7aa5b558c
faridvaliyev1 <faridvaliyevgit@gmail.com>
2020-12-06 22:17:04 +0200

    Adding booking controller finish method

--- lib/agileparking_web/controllers/booking_controller.ex
+++ lib/agileparking_web/controllers/booking_controller.ex
@@ -1,13 +1,40 @@
 defmodule AgileparkingWeb.BookingController do
   use AgileparkingWeb, :controller
-  import Ecto.Query
+  import Ecto.Query, only: [from: 2]
+  alias Ecto.{Changeset, Multi}
   alias Agileparking.Repo
+  alias Agileparking.Bookings
   alias Agileparking.Bookings.Booking
+  alias Agileparking.Sales.Zone
 
   def index(conn, _params) do
     user = Agileparking.Authentication.load_current_user(conn)
     bookings = Repo.all(from b in Booking, where: b.user_id == ^user.id)
     render conn, "index.html", bookings: bookings
   end
+
+  def show(conn,%{"id"=>id}) do
+    booking=Bookings.get_booking!(id)
+
+    render(conn,"show.html",booking: booking)
+  end
+
+  def delete(conn,%{"id"=>id}) do
+    booking=Bookings.get_booking!(id)
+
+
+    Repo.get_by(Zone,available: false)
+             |> Ecto.Changeset.change(%{available: true})
+             |>Repo.update()
+
+    {:ok, _booking} = Bookings.delete_booking(booking)
+    conn
+    |> put_flash(:info,"Booking deleted succesfully")
+    |> redirect(to: Routes.booking_path(conn,:index))
+  end
+
+
+
+
 
 end
